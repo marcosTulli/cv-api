d1885006df7938a20d9a7cc3be12738bf99ea91a
Marcos <marcostulli@hotmail.com>
2024-06-17 23:04:05 -0300

    Add: api key requirement

--- midleware/apiKey.js
+++ midleware/apiKey.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
 const bcrypt = require('bcrypt');
-
 const saltRounds = 10;
+
 const validApiKey = process.env.API_KEY;
 
 const apiKeyMiddleware = async (req, res, next) => {
     const incomingKey = req.header('x-api-key');
+    const hashed = await bcrypt.hash(validApiKey, saltRounds).finally((hash) => { return hash; });
+
     if (!incomingKey) {
         return res.status(401).send({ message: 'API key is missing' });
     }
-    const isApiKeyValid = await bcrypt.compare(incomingKey, validApiKey).finally((result) => result);
+
+    const isApiKeyValid = await bcrypt.compare(validApiKey, incomingKey).finally((result) => { result; });
 
     if (isApiKeyValid) {
-        next(); // API key is valid, proceed to the next middleware or route handler
+        next();
     } else {
         res.status(401).send({ message: 'Invalid API key' });
     }
