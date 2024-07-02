5df338f87e67162128089d8b1d8959afa8b3459b
Marcos <marcostulli@hotmail.com>
2024-07-02 16:54:45 -0300

    Fix double repsonse send

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,14 +2,16 @@ const { clientConnect } = require('../utils');
 
 const baseController = async (req, res, collectionName) => {
     const base = req.baseUrl;
-    if (base.includes('icons')) {
-        res.status(404).send({ error: 'Missing file name' });
-    }
     try {
-        await clientConnect();
-        const collection = db.collection(collectionName);
-        const data = await collection.find().toArray();
-        res.status(200).send(data);
+        if (base.includes('icons')) {
+            res.status(404).send({ error: 'Missing file name' });
+        }
+        else {
+            await clientConnect();
+            const collection = db.collection(collectionName);
+            const data = await collection.find().toArray();
+            res.status(200).send(data);
+        }
     } catch (error) {
         console.error(error.stack);
         res.status(500).send({ error, message: 'Internal Server Error' });
